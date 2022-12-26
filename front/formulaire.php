5a23614f298ea21607aa0e1c1284c93d0a1211de
HayZun <paul.durieux0408@gmail.com>
2022-12-26 17:11:39 +0100

    ea

--- front/formulaire.php
+++ front/formulaire.php
@@ -175,10 +175,15 @@ if (strlen($response) == 71) {
     foreach($req as $row) {
      $count = $row["COUNT(*)"];
     }
-    echo $count;
     if (0 == $count) {
       //insert datas
       $req = $DB->query("INSERT INTO `glpi_plugin_cartosi_app` (`name`, `description`,`domain`,`leader`,`check`) VALUES ('$name','$description', '$domain','$teamleader','$datecheck')");
+    } else {
+      $bool = false;
+      $req = $DB->query("SELECT `Name` FROM glpi_plugin_cartosi_app");
+      foreach($req as $row){
+         echo $row + "<br>";
+      }
     } 
    }
 }
