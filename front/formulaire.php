e7ebfb5608098f02e9b099a541ee25b62ec3e375
HayZun <paul.durieux0408@gmail.com>
2022-12-26 15:58:26 +0100

    data

--- front/formulaire.php
+++ front/formulaire.php
@@ -175,7 +175,7 @@ if (strlen($response) == 71) {
     //insert datas
     //$req = $DB->query("INSERT INTO `glpi_plugin_cartosi_credentials` (`id`, `token`, `tenant`) VALUES (1, '".$token."', ".$tenant.")");
     //$req = $DB->query("INSERT INTO `glpi_plugin_cartosi_app` (`name`, `description`, `domain`,`leader`, `check`) VALUES (".$name.", ".$description.", ".$domain.",".$teamleader.", ".$datacheck.")");
-    $req = $DB->query("INSERT INTO `glpi_plugin_cartosi_app` (`name`, `description`,`domain`,`leader`,`check`) VALUES ('$name','$description', '$domain','$teamleader','2021-06-08')");
+    $req = $DB->query("INSERT INTO `glpi_plugin_cartosi_app` (`name`, `description`,`domain`,`leader`,`check`) VALUES ('$name','$description', '$domain','$teamleader','$datacheck')");
    }
 }
 
