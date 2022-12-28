ce2b2f1ae5352fae9112bf089423dd248206edb6
HayZun <paul.durieux0408@gmail.com>
2022-12-28 15:52:16 +0100

    aa

--- hook.php
+++ hook.php
@@ -35,6 +35,67 @@
 
 use GlpiPlugin\Example\Example;
 
+// Hook called on profile change
+// Good place to evaluate the user right on this plugin
+// And to save it in the session
+function plugin_change_profile_example() {
+   // For example : same right of computer
+   if (Session::haveRight('computer', UPDATE)) {
+      $_SESSION["glpi_plugin_example_profile"] = ['example' => 'w'];
+
+   } else if (Session::haveRight('computer', READ)) {
+      $_SESSION["glpi_plugin_example_profile"] = ['example' => 'r'];
+
+   } else {
+      unset($_SESSION["glpi_plugin_example_profile"]);
+   }
+}
+
+
+// Define dropdown relations
+function plugin_example_getDatabaseRelations() {
+   return ["glpi_plugin_example_dropdowns" => ["glpi_plugin_example" => "plugin_example_dropdowns_id"]];
+}
+
+
+// Define Dropdown tables to be manage in GLPI :
+function plugin_example_getDropdown() {
+   // Table => Name
+   return [Dropdown::class => __("Plugin Example Dropdown", 'example')];
+}
+
+
+
+////// SEARCH FUNCTIONS ///////(){
+
+// Define Additionnal search options for types (other than the plugin ones)
+function plugin_example_getAddSearchOptions($itemtype) {
+   $sopt = [];
+   if ($itemtype == 'Computer') {
+         // Just for example, not working...
+         $sopt[1001]['table']     = 'glpi_plugin_example_dropdowns';
+         $sopt[1001]['field']     = 'name';
+         $sopt[1001]['linkfield'] = 'plugin_example_dropdowns_id';
+         $sopt[1001]['name']      = __('Example plugin', 'example');
+   }
+   return $sopt;
+}
+
+function plugin_example_getAddSearchOptionsNew($itemtype) {
+   $options = [];
+   if ($itemtype == 'Computer') {
+      //Just for example, not working
+      $options[] = [
+         'id'        => '1002',
+         'table'     => 'glpi_plugin_example_dropdowns',
+         'field'     => 'name',
+         'linkfield' => 'plugin_example_dropdowns_id',
+         'name'      => __('Example plugin new', 'example')
+      ];
+   }
+   return $options;
+}
+
 // See also GlpiPlugin\Example\Example::getSpecificValueToDisplay()
 function plugin_example_giveItem($type, $ID, $data, $num) {
    $searchopt = &Search::getOptions($type);
@@ -53,6 +114,141 @@ function plugin_example_giveItem($type, $ID, $data, $num) {
    }
    return "";
 }
+
+
+function plugin_example_displayConfigItem($type, $ID, $data, $num) {
+   $searchopt = &Search::getOptions($type);
+   $table     = $searchopt[$ID]["table"];
+   $field     = $searchopt[$ID]["field"];
+
+   // Example of specific style options
+   // No need of the function if you do not have specific cases
+   switch ($table.'.'.$field) {
+      case "glpi_plugin_example_examples.name" :
+         return " style=\"background-color:#DDDDDD;\" ";
+   }
+   return "";
+}
+
+
+function plugin_example_addDefaultJoin($type, $ref_table, &$already_link_tables) {
+   // Example of default JOIN clause
+   // No need of the function if you do not have specific cases
+   switch ($type) {
+      //       case Example::class :
+      case "MyType" :
+         return Search::addLeftJoin($type, $ref_table, $already_link_tables,
+                                    "newtable", "linkfield");
+   }
+   return "";
+}
+
+
+function plugin_example_addDefaultSelect($type) {
+   // Example of default SELECT item to be added
+   // No need of the function if you do not have specific cases
+   switch ($type) {
+      //       case Example::class :
+      case "MyType" :
+         return "`mytable`.`myfield` = 'myvalue' AS MYNAME, ";
+   }
+   return "";
+}
+
+
+function plugin_example_addDefaultWhere($type) {
+   // Example of default WHERE item to be added
+   // No need of the function if you do not have specific cases
+   switch ($type) {
+      //       case Example::class :
+      case "MyType" :
+         return " `mytable`.`myfield` = 'myvalue' ";
+   }
+   return "";
+}
+
+
+function plugin_example_addLeftJoin($type, $ref_table, $new_table, $linkfield) {
+   // Example of standard LEFT JOIN  clause but use it ONLY for specific LEFT JOIN
+   // No need of the function if you do not have specific cases
+   switch ($new_table) {
+      case "glpi_plugin_example_dropdowns" :
+         return " LEFT JOIN `$new_table` ON (`$ref_table`.`$linkfield` = `$new_table`.`id`) ";
+   }
+   return "";
+}
+
+
+function plugin_example_forceGroupBy($type) {
+   switch ($type) {
+      case Example::class :
+         // Force add GROUP BY IN REQUEST
+         return true;
+   }
+   return false;
+}
+
+
+function plugin_example_addWhere($link, $nott, $type, $ID, $val, $searchtype) {
+   $searchopt = &Search::getOptions($type);
+   $table     = $searchopt[$ID]["table"];
+   $field     = $searchopt[$ID]["field"];
+
+   $SEARCH = Search::makeTextSearch($val, $nott);
+
+   // Example of standard Where clause but use it ONLY for specific Where
+   // No need of the function if you do not have specific cases
+   switch ($table.".".$field) {
+      /*case "glpi_plugin_example.name" :
+        $ADD = "";
+        if ($nott && $val!="NULL") {
+           $ADD = " OR `$table`.`$field` IS NULL";
+        }
+        return $link." (`$table`.`$field` $SEARCH ".$ADD." ) ";*/
+      case "glpi_plugin_example_examples.serial" :
+          return $link." `$table`.`$field` = '$val' ";
+   }
+   return "";
+}
+
+
+// This is not a real example because the use of Having condition in this case is not suitable
+function plugin_example_addHaving($link, $nott, $type, $ID, $val, $num) {
+   $searchopt = &Search::getOptions($type);
+   $table     = $searchopt[$ID]["table"];
+   $field     = $searchopt[$ID]["field"];
+
+   $SEARCH = Search::makeTextSearch($val, $nott);
+
+   // Example of standard Having clause but use it ONLY for specific Having
+   // No need of the function if you do not have specific cases
+   switch ($table.".".$field) {
+      case "glpi_plugin_example.serial" :
+         $ADD = "";
+         if (($nott && $val!="NULL")
+             || $val == '^$') {
+            $ADD = " OR ITEM_$num IS NULL";
+         }
+         return " $LINK ( ITEM_".$num.$SEARCH." $ADD ) ";
+   }
+   return "";
+}
+
+
+function plugin_example_addSelect($type, $ID, $num) {
+   $searchopt = &Search::getOptions($type);
+   $table     = $searchopt[$ID]["table"];
+   $field     = $searchopt[$ID]["field"];
+
+   // Example of standard Select clause but use it ONLY for specific Select
+   // No need of the function if you do not have specific cases
+   // switch ($table.".".$field) {
+   //    case "glpi_plugin_example.name" :
+   //       return $table.".".$field." AS ITEM_$num, ";
+   // }
+   return "";
+}
+
 
 function plugin_example_addOrderBy($type, $ID, $order, $key = 0) {
    $searchopt = &Search::getOptions($type);
