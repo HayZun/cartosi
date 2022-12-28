ca20f0646b1c72e8688b132f09a8ea5fb718b567
HayZun <paul.durieux0408@gmail.com>
2022-12-28 15:25:20 +0100

    aa

--- front/config.form.php
+++ front/config.form.php
@@ -45,7 +45,7 @@ foreach($req as $row) {
      $count = $row["COUNT(*)"];
 }
 if (0 == $count) {
-    echo '<form method="post" action="config.php">';
+    echo '<form method="post" action="formulaire.php">';
     echo 'Token API : ';
     echo '<input type="nombre" id="token" name="token" placeholder="Entrer le token de API carto-si"  size="50">';
     echo '<td style="width: 200px">' . __('     Tenant :       ') .'</td>';
