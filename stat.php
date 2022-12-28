e01094d2f8cb3d5125bd419d6036900080ffbbcc
HayZun <paul.durieux0408@gmail.com>
2022-12-28 09:59:44 +0100

    aa

---
+++ stat.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * -------------------------------------------------------------------------
+ * Example plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of Example.
+ *
+ * Example is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Example is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Example. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2006-2022 by Example plugin team.
+ * @license   GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/pluginsGLPI/example
+ * -------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Original Author of file:
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+// Non menu entry case
+//header("Location:../../central.php");
+
+// Entry menu case
+define('GLPI_ROOT', '../..');
+include (GLPI_ROOT . "/inc/includes.php");
+
+Session::checkRight("config", "w");
+
+Html::header("TITLE", $_SERVER['PHP_SELF'], "plugins");
+
+echo "This is the plugin stat page";
+
+Html::footer();
