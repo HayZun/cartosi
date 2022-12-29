e3cac2965cbd960fa768b9600f2e52618dd65407
HayZun <paul.durieux0408@gmail.com>
2022-12-29 10:59:47 +0100

    aa

--- src/Example.php
+++ src/Example.php
@@ -209,6 +209,29 @@ class Example extends CommonDBTM {
         }
          //test connexion
          $curl = curl_init();
+         curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://app.carto-si.com/api/v2/activity/',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+               'Authorization: Bearer {"myTenant":{"id":"'.$tenant.'"},"token":"'.$token.'"}'
+            ),
+            ));
+   
+            $response = curl_exec($curl);
+            curl_close($curl);
+            if (strlen($response) == 71) {
+               $task->log("Arrêt de la synchronisation");
+               $task->log("Tenant ou token invalide");
+            } else {
+               $task->log("Token/tenant valide");
+            }
       }
       return 1;
+   }  
 }
