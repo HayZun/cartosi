7915c32ecd98bd471b0d8a85cb3a769fcd92955d
HayZun <paul.durieux0408@gmail.com>
2022-12-29 10:58:47 +0100

    aa

--- src/Example.php
+++ src/Example.php
@@ -209,19 +209,6 @@ class Example extends CommonDBTM {
         }
          //test connexion
          $curl = curl_init();
-         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://app.carto-si.com/api/v2/activity/',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_HTTPHEADER => array(
-               'Authorization: Bearer {"myTenant":{"id":"'.$tenant.'"},"token":"'.$token.'"}'
-            ),
-            ));
       }
       return 1;
 }
