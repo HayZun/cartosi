a2cd99f9c1d916e2e950ccd740cc4d554a30a4a6
HayZun <paul.durieux0408@gmail.com>
2022-12-29 11:00:30 +0100

    aa

--- src/Example.php
+++ src/Example.php
@@ -193,45 +193,5 @@ class Example extends CommonDBTM {
       global $DB;
       
       $task->log("Initalisation synchro cartoSI");
-
-      //get-VARs
-      $req = $DB->query('SELECT COUNT(*) FROM glpi_plugin_cartosi_credentials');
-      foreach($req as $row) {
-         $count = $row["COUNT(*)"];
-      }
-
-      //
-      if (1 == $count) {
-         $req = $DB->query('SELECT * FROM glpi_plugin_cartosi_credentials');
-        foreach($req as $row) {
-          $token = $row["token"];
-          $tenant = $row["tenant"];
-        }
-         //test connexion
-         $curl = curl_init();
-         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://app.carto-si.com/api/v2/activity/',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_HTTPHEADER => array(
-               'Authorization: Bearer {"myTenant":{"id":"'.$tenant.'"},"token":"'.$token.'"}'
-            ),
-            ));
-   
-            $response = curl_exec($curl);
-            curl_close($curl);
-            if (strlen($response) == 71) {
-               $task->log("Arrêt de la synchronisation");
-               $task->log("Tenant ou token invalide");
-            } else {
-               $task->log("Token/tenant valide");
-            }
-      }
-      return 1;
    }  
 }
