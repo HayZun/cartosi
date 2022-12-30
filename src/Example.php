34fca9807c274d5d9e3ecf747cb553000857b397
HayZun <paul.durieux0408@gmail.com>
2022-12-30 16:14:59 +0100

    aa

--- src/Example.php
+++ src/Example.php
@@ -315,4 +315,50 @@ class Example extends CommonDBTM {
          }
          return 1;
       }
+
+   function getTabNameForItem(CommonGLPI $item, $withtemplate=0) {
+      switch ($item::getType()) {
+         case Ticket::getType():
+         case Computer::getType():
+         case Phone::getType():
+            return __('Tab from my plugin', 'example');
+            break;
+      }
+      return '';
+   }
+   
+   static function displayTabContentForItem(CommonGLPI $item, $tabnum=1, $withtemplate=0) {
+      switch ($item::getType()) {
+         case Ticket::getType():
+            //display form for computers
+            self::displayTabContentForTicket($item);
+            break;
+         case Phone::getType():
+            self::displayTabContentForPhone($item);
+            break;
+      }
+      if ($item->getType() == 'ObjetDuCoeur') {
+         $monplugin = new self();
+         $ID = $item->getField('id');
+         // j'affiche le formulaire
+         $monplugin->nomDeLaFonctionQuiAfficheraLeContenuDeMonOnglet();
+      }
+      return true;
+   }
+
+   private static function displayTabContentForTicket(Ticket $item) {
+      //...
+   }
+
+   private static function displayTabContentForComputer(Computer $item) {
+      //...
+   }
+   
+   private static function displayTabContentForPhone(Phone $item) {
+      //...
+   }
+
+   private static function nomDeLaFonctionQuiAfficheraLeContenuDeMonOnglet() {
+      echo "toto";
+   }
 }
