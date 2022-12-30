e9b4d3732ed1fed4ab6ca2331bd17a3b84115d7a
HayZun <paul.durieux0408@gmail.com>
2022-12-30 16:10:27 +0100

    aa

--- setup.php
+++ setup.php
@@ -63,6 +63,11 @@ function plugin_init_example() {
    $PLUGIN_HOOKS['config_page']['example'] = 'front/config.php';
    // CSRF compliance : All actions must be done via POST and forms closed by Html::closeForm();
    $PLUGIN_HOOKS[Hooks::CSRF_COMPLIANT]['example'] = true;
+
+   //add tab
+   Plugin::registerClass(
+      'example'
+   )
 }
 
 /**
