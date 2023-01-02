718aa0f55764a1f642357a361a7e2ca7fdc5bfb9
HayZun <paul.durieux0408@gmail.com>
2023-01-02 14:42:57 +0100

    aa

--- front/example.php
+++ front/example.php
@@ -33,7 +33,7 @@
 // Purpose of file:
 // ----------------------------------------------------------------------
 
-use GlpiPlugin\Example\Example;
+//use GlpiPlugin\Example\Example;
 
 include ('../../../inc/includes.php');
 
