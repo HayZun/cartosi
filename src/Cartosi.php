08a024ce3945c7813983a91d05550bb44fe71be4
HayZun <paul.durieux0408@gmail.com>
2023-01-03 14:32:54 +0100

    aa

--- src/Cartosi.php
+++ src/Cartosi.php
@@ -179,7 +179,7 @@ class Cartosi extends CommonDBTM {
 
       switch ($name) {
          case 'CartoSI' :
-            return ['description' => __('Synchronisation application CartoSI à GLPI', 'example')];
+            return ['description' => __('Synchronisation application CartoSI à GLPI', 'cartosi')];
       }
       return [];
    }
