88375874a3c3d87ccb52e15acfe700be01e405b8
HayZun <paul.durieux0408@gmail.com>
2023-01-04 18:12:39 +0100

    aa

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -46,9 +46,6 @@ class PluginCartosiTicket extends CommonDBTM {
       echo "<h1>Applications Carto-SI :</h1>";
       echo "Polo";
       echo "</center>";
-      echo "<script type="text/javascript">";
-      echo 'alert("This alert box was called with the onload event");'
-      echo "</script>";
    }
 
 
