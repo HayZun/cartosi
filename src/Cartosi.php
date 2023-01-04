98bb29af5be69513cdf92301bf4dd77dbcba3493
HayZun <paul.durieux0408@gmail.com>
2023-01-04 16:13:17 +0100

    aa

--- src/Cartosi.php
+++ src/Cartosi.php
@@ -306,7 +306,7 @@ class Cartosi extends CommonDBTM {
                } else {
                   $task->log("update");
                   //application exists
-                  $req = $DB->query("UPDATE glpi_plugin_cartosi_cartosis SET `name`='".$name."' WHERE id_app='".$idapp"'");
+                  //$req = $DB->query("UPDATE glpi_plugin_cartosi_cartosis SET `name`='".$name."' WHERE id_app='".$idapp"'");
                   $task->log("UPDATE `glpi_plugin_cartosi_cartosis` 
                   SET `name` = $name,
                       `description` = $description,
