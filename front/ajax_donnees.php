7dc8ae44631275859bb788becca3a38f6d0035de
HayZun <paul.durieux0408@gmail.com>
2023-01-09 14:39:55 +0100

    aa

--- front/ajax_donnees.php
+++ front/ajax_donnees.php
@@ -1,4 +1,10 @@
 <?php
+// Entry menu case
+define('GLPI_ROOT', '../..');
+include (GLPI_ROOT . "/inc/includes.php");
+
+Session::checkRight("config", "w");
+
 function testajax(){
     echo "coucou ajax";
 }
