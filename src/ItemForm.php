1eecb77bea844373a484ce226cdf609901076361
HayZun <paul.durieux0408@gmail.com>
2023-01-10 15:53:22 +0100

    aa

--- src/ItemForm.php
+++ src/ItemForm.php
@@ -53,6 +53,9 @@ class ItemForm {
          case "Ticket":
             foreach($item as $key => $element) {
                echo $key . " - " . $element."<br />";
+               foreach($element as $keys => $elements) {
+                  echo $keys . " - " . $elements."<br />";
+               }
             }
             echo 'Carto-SI <input type="text" value="toto" size=2/>';
       }
