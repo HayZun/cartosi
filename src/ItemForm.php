cb7625ccd51098c0ccd288887052ffc2870c65d8
HayZun <paul.durieux0408@gmail.com>
2023-01-10 14:56:02 +0100

    aa

--- src/ItemForm.php
+++ src/ItemForm.php
@@ -51,7 +51,6 @@ class ItemForm {
       $options = $params['options'];
 
       $firstelt = ($item::getType() == Ticket::class ? 'th' : 'td');
-      echo 'Carto-SI';
-      echo '<input type="text" name="example_post_form_hook" id="example_post_form_hook"/>';
+      echo 'Carto-SI <input type="text" name="example_post_form_hook" id="example_post_form_hook"/>';
    }
 }
