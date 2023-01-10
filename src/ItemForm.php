48c2af8e86c457f99c919d5376248441c8845334
HayZun <paul.durieux0408@gmail.com>
2023-01-10 16:03:14 +0100

    aa

--- src/ItemForm.php
+++ src/ItemForm.php
@@ -54,6 +54,7 @@ class ItemForm {
             foreach($item as $key => $element) {
                foreach($element as $keys => $elements) {
                   echo $keys;
+                  echo "<br>";
                   if(strcmp($keys,"id")) {
                      echo $keys . " - " . $elements."<br />";
                   }
