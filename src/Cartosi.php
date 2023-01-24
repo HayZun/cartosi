e2e867f116ef388987d31b594131be3b545cd509
HayZun <paul.durieux0408@gmail.com>
2023-01-24 14:47:55 +0100

    aa

--- src/Cartosi.php
+++ src/Cartosi.php
@@ -362,7 +362,7 @@ class Cartosi extends CommonDBTM {
       $response = curl_exec($curl);
       $task->log($response);
       $task->log(gettype($response));
-      $data = json_decode($response, true)
+      $data = json_decode($response, true);
       $task->log(var_dump($data));
 
       curl_close($curl);
